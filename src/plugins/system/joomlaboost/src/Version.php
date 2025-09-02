3545ca672056ce47c4640df133da5a91c099d5df
Bojan Pavlovic <bojan.creator@gmail.com>
2025-09-02 15:51:32 +0200

    git: quick push

--- src/plugins/system/joomlaboost/src/Version.php
+++ src/plugins/system/joomlaboost/src/Version.php
@@ -12,48 +12,48 @@ namespace JoomlaBoost\Plugin\System\JoomlaBoost;
  */
 final class Version
 {
-    /**
-     * Plugin version
-     */
-    public const PLUGIN_VERSION = '0.1.16-clean';
+  /**
+   * Plugin version
+   */
+  public const PLUGIN_VERSION = '0.1.16-clean';
 
-    /**
-     * Plugin name
-     */
-    public const PLUGIN_NAME = 'JoomlaBoost';
+  /**
+   * Plugin name
+   */
+  public const PLUGIN_NAME = 'JoomlaBoost';
 
-    /**
-     * Plugin full title
-     */
-    public const PLUGIN_TITLE = 'JoomlaBoost - Performance & SEO Optimization';
+  /**
+   * Plugin full title
+   */
+  public const PLUGIN_TITLE = 'JoomlaBoost - Performance & SEO Optimization';
 
-    /**
-     * Release date
-     */
-    public const RELEASE_DATE = '2025-09-02';
+  /**
+   * Release date
+   */
+  public const RELEASE_DATE = '2025-09-02';
 
-    /**
-     * Get formatted version string for debug output
-     *
-     * @return string
-     */
-    public static function getDebugString(): string
-    {
-        return self::PLUGIN_NAME . ' v' . self::PLUGIN_VERSION;
-    }
+  /**
+   * Get formatted version string for debug output
+   *
+   * @return string
+   */
+  public static function getDebugString(): string
+  {
+    return self::PLUGIN_NAME . ' v' . self::PLUGIN_VERSION;
+  }
 
-    /**
-     * Get full version information array
-     *
-     * @return array<string, string>
-     */
-    public static function getVersionInfo(): array
-    {
-        return [
-            'name' => self::PLUGIN_NAME,
-            'version' => self::PLUGIN_VERSION,
-            'title' => self::PLUGIN_TITLE,
-            'release_date' => self::RELEASE_DATE,
-        ];
-    }
+  /**
+   * Get full version information array
+   *
+   * @return array<string, string>
+   */
+  public static function getVersionInfo(): array
+  {
+    return [
+      'name' => self::PLUGIN_NAME,
+      'version' => self::PLUGIN_VERSION,
+      'title' => self::PLUGIN_TITLE,
+      'release_date' => self::RELEASE_DATE,
+    ];
+  }
 }
--- src/plugins/system/offroadseo/src/Version.php
+++ src/plugins/system/offroadseo/src/Version.php
@@ -12,48 +12,48 @@ namespace Offroad\Plugin\System\Offroadseo;
  */
 final class Version
 {
-    /**
-     * Plugin version
-     */
-    public const PLUGIN_VERSION = '1.8.8';
+  /**
+   * Plugin version
+   */
+  public const PLUGIN_VERSION = '1.8.8';
 
-    /**
-     * Plugin name
-     */
-    public const PLUGIN_NAME = 'OffroadSEO';
+  /**
+   * Plugin name
+   */
+  public const PLUGIN_NAME = 'OffroadSEO';
 
-    /**
-     * Plugin full title
-     */
-    public const PLUGIN_TITLE = 'OffroadSerbia - SEO Plugin';
+  /**
+   * Plugin full title
+   */
+  public const PLUGIN_TITLE = 'OffroadSerbia - SEO Plugin';
 
-    /**
-     * Release date
-     */
-    public const RELEASE_DATE = '2025-09-02';
+  /**
+   * Release date
+   */
+  public const RELEASE_DATE = '2025-09-02';
 
-    /**
-     * Get formatted version string for debug output
-     *
-     * @return string
-     */
-    public static function getDebugString(): string
-    {
-        return self::PLUGIN_NAME . ' v' . self::PLUGIN_VERSION;
-    }
+  /**
+   * Get formatted version string for debug output
+   *
+   * @return string
+   */
+  public static function getDebugString(): string
+  {
+    return self::PLUGIN_NAME . ' v' . self::PLUGIN_VERSION;
+  }
 
-    /**
-     * Get full version information array
-     *
-     * @return array<string, string>
-     */
-    public static function getVersionInfo(): array
-    {
-        return [
-            'name' => self::PLUGIN_NAME,
-            'version' => self::PLUGIN_VERSION,
-            'title' => self::PLUGIN_TITLE,
-            'release_date' => self::RELEASE_DATE,
-        ];
-    }
+  /**
+   * Get full version information array
+   *
+   * @return array<string, string>
+   */
+  public static function getVersionInfo(): array
+  {
+    return [
+      'name' => self::PLUGIN_NAME,
+      'version' => self::PLUGIN_VERSION,
+      'title' => self::PLUGIN_TITLE,
+      'release_date' => self::RELEASE_DATE,
+    ];
+  }
 }
